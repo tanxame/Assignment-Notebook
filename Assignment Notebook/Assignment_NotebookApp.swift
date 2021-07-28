19a9c33d8a3f3881d4fd23fa449952ffab0e8404
Amelia <atan@ucls.uchicago.edu>
2021-07-28 12:00:46 -0500

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Franklin Tan on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
